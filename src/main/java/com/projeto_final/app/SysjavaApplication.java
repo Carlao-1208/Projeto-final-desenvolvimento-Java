7006960aa624c0a3bcf25f50aa8e33bff614dbc4
carlao-1208 <carlosbob27@gmail.com>
2026-02-24 19:35:02 -0300

    Projeto final desenvolvedor Java

---
+++ src/main/java/com/projeto_final/app/SysjavaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_final.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_final/app/SysjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_final.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
